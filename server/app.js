7c7b59b54b7e2cb5b2aafc6023dd1d58a74f553d
Vie <the_ruesi@zoho.com>
2020-06-30 08:04:40 +0100

    test deploy

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 9000
