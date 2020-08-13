88fc5c471d9e1f9f357d70f9186ff20cff4a0449
Vie <the_ruesi@zoho.com>
2020-08-13 19:26:59 +0100

    ongoing responsive design with media queries

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -18,9 +18,6 @@ const HomePage = (props) => {
         <h2 className="thumb-list-title">Recipes</h2>
         <ThumbList />
       </section>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2880 48" fill="none">
-      <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
-    </svg>
     </div>
 
   
