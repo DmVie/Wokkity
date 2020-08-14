60aba3784498fa4eae7b8bb14ee100ec92fbe085
Vie <the_ruesi@zoho.com>
2020-08-14 09:44:30 +0100

    cont responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,15 @@ import './App.scss';
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { faInfoCircle, faWindowClose, faStar as 
-  faStarSolid, faQuoteRight, faQuoteLeft, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+  faStarSolid, faQuoteRight, faQuoteLeft, faUserAstronaut, faPepperHot, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faCommentDots, faClipboard } from '@fortawesome/free-regular-svg-icons';
 import Footer  from '../Footer/Footer'
 import AppRouter from '../../routers/AppRouter';
 
 import { createBrowserHistory } from 'history';
 export const history = createBrowserHistory();
 
-library.add(faInfoCircle, faWindowClose, faStar, faStarSolid, faQuoteLeft, faQuoteRight, faUserAstronaut)
+library.add(faInfoCircle, faWindowClose, faStar, faCommentDots, faClipboard, faStarSolid, faQuoteLeft, faQuoteRight, faUserAstronaut, faPepperHot, faHome)
 
 function App() {
   return (
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer>
-    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+  
     </footer>
   )
 }
