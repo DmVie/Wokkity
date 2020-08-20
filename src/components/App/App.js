307e2a028b739de23395b950d92ca61c361619a1
Vie <the_ruesi@zoho.com>
2020-08-20 21:18:50 +0100

    font awesome menu icon added to lib

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,8 @@ import './App.scss';
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { faInfoCircle, faWindowClose, faStar as 
-  faStarSolid, faQuoteRight, faQuoteLeft, faUserAstronaut, faPepperHot, faHome } from '@fortawesome/free-solid-svg-icons'
+  faStarSolid, faQuoteRight, faQuoteLeft, faUserAstronaut, faPepperHot, faHome, faBars
+ } from '@fortawesome/free-solid-svg-icons'
 import { faStar, faCommentDots, faClipboard } from '@fortawesome/free-regular-svg-icons';
 import Footer  from '../Footer/Footer'
 import AppRouter from '../../routers/AppRouter';
@@ -12,7 +13,7 @@ import AppRouter from '../../routers/AppRouter';
 import { createBrowserHistory } from 'history';
 export const history = createBrowserHistory();
 
-library.add(faInfoCircle, faWindowClose, faStar, faCommentDots, faClipboard, faStarSolid, faQuoteLeft, faQuoteRight, faUserAstronaut, faPepperHot, faHome)
+library.add(faInfoCircle, faWindowClose, faStar, faCommentDots, faClipboard, faStarSolid, faQuoteLeft, faQuoteRight, faUserAstronaut, faPepperHot, faHome, faBars)
 
 function App() {
   return (
